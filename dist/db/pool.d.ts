cd6bf6f30c8ae17f05123d0a794ae0d783a41e7a
Glitch <themar3machine@gmail.com>
2025-08-26 03:00:54 +0200

    Remove newly ignored files

--- dist/db/pool.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    query: (sqlString: string, params?: [any]) => Promise<import("pg").QueryResult<any>>;
-};
-export default _default;
-//# sourceMappingURL=pool.d.ts.map
